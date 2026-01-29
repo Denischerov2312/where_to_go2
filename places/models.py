71a8b22d6c2c5760d2496a4bf42703e1844630c5
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-01-29 16:23:07 +0300

    add image model

--- places/models.py
+++ places/models.py
@@ -6,6 +6,15 @@ class Place(models.Model):
     description_short = models.TextField()
     description_long = models.TextField()
     coordinates = models.JSONField(default=dict)
-    
+
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    number = models.IntegerField()
+    name = models.CharField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return f"{self.number} {self.name}"
