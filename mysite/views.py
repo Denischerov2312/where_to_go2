ef0e9e3431be436bbe74aeda3fba38649a8a9482
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-02-03 19:09:08 +0300

    Выгрузил данные из шаблона

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,42 @@ from django.template import loader
 
 
 def show(request):
+    geo_json = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "static/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "static/places/roofs24.json"
+          }
+        }
+      ]
+    }
     template = loader.get_template('index.html')
-    context = {}
+    context = {
+                'geo_json': geo_json,
+                'sidebar_js_filepath': 'leaflet-sidebar.js',
+                'favicon_filepath': 'favicon.png',
+                'sidebar_css_filepath': 'leaflet-sidebar.css',
+                'svg_filepath': 'hand-pointer-regular.svg',
+               }
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
