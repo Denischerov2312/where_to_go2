2d21c118c804b5ff6e184f48cfa2a44c367eb996
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-02-17 20:10:36 +0300

    Добавил preview в админу для картинок

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.html import format_html
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['image_preview']
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-width: 200px; max-height: 200px; object-fit: contain;" />',
+                obj.image.url
+            )
+        return '-'
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Place)
@@ -11,6 +26,3 @@ class PlaceAdmin(admin.ModelAdmin):
     inlines = [
         ImageInline,
     ]
-
-
-admin.site.register(Image)
