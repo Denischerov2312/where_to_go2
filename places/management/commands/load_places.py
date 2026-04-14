daa380b5e5fca084ba3d475794cb4881f5064bac
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-04-14 19:06:45 +0300

    Добавил функцию загрузки json локакий с репозиторий, чере команду

--- places/management/commands/load_places.py
+++ places/management/commands/load_places.py
@@ -1,16 +1,32 @@
 import json
+import requests
 from pathlib import Path
 from django.core.management.base import BaseCommand
 from places.models import Place, Image
+from os.path import join
+from os import makedirs
 
 
-## TODO Добавить через request загрузку данных
 
 class Command(BaseCommand):
-    help = "Загружает файлы json в базу данных"
-    
+    help = "Загружает файлы json и данные из них в базу данных"
+    saved_path = 'static/places_json'
+
+    def download_github_json(self, owner='devmanorg', repo='where-to-go-places', download_dirs='places'):
+        github_url = join(f'https://api.github.com/repos/{owner}/{repo}/contents/',  download_dirs)
+        response = requests.get(github_url)
+        response.raise_for_status()
+        files = response.json()
+        for file_info in files:
+            if file_info['name'].endswith('.json'):
+                file_data = requests.get(file_info['download_url']).json()
+                makedirs(self.saved_path, exist_ok=True)
+                with open(join(self.saved_path, file_info['name']), 'w', encoding='utf-8') as file:
+                    json.dump(file_data, file, ensure_ascii=False)
+
     def handle(self, *args, **options):
-        
+        self.download_github_json()
+
         for file_path in Path('static/places_json').iterdir():
             if file_path.is_file():
                 with open(file_path, 'r', encoding='utf-8') as file:
