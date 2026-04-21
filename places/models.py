b57edeff8745dcde366a8dc19a03637738dd2f35
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-04-21 18:47:30 +0300

    Сделал поля необязательными, и добавил миграцию

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Название места', max_length=150)
-    short_description = models.TextField('Короткое описание')
-    long_description = HTMLField('Длинное описание')
+    short_description = models.TextField('Короткое описание', blank=True)
+    long_description = HTMLField('Длинное описание', blank=True)
     longitude = models.FloatField('Долгота')
     latitude = models.FloatField('Широта')
 
