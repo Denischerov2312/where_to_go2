d8f8dfd3a8da8a376a6ba624ec2259cc54169299
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-04-21 18:38:58 +0300

    Исправил имя переменной в правильном порядке

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Название места', max_length=150)
-    description_short = models.TextField('Короткое описание')
-    description_long = HTMLField('Длинное описание')
+    short_description = models.TextField('Короткое описание')
+    long_description = HTMLField('Длинное описание')
     longitude = models.FloatField('Долгота')
     latitude = models.FloatField('Широта')
 
