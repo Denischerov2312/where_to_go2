c3938d4bbb451c41fc8342868064c2c5aaf39765
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-05-10 13:51:29 +0300

    перешел с show на start page, исправил баг отображения картинов

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show),
+    path('', views.start_page),
     path('places/<int:place_id>/', views.show_place, name='parse_place_details'),
     path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- places/views.py
+++ places/views.py
@@ -26,59 +26,27 @@ def start_page(request):
         } for place in places
     ]
     context = {
-        'places': {
+        'geo_json': {
             'type': 'FeatureCollection',
             'features': features
         }
     }
-    return render(request, 'index.html', context)
+    template = loader.get_template('index.html')
+    rendered_page = template.render(context, request)
+    return HttpResponse(rendered_page)
 
 
 def show_place(request, place_id):
     place = get_object_or_404(Place.objects.prefetch_related('images'), id=place_id)
     images_urls = [image.image.url for image in place.images.all()]
-
     payload = {
         'title': place.title,
         'imgs': images_urls,
-        'short_description': place.short_description,
-        'long_description': place.long_description,
+        'description_short': place.short_description,
+        'description_long': place.long_description,
         'coordinates': {
             'lng': place.longitude,
             'lat': place.latitude,
         }
     }
     return JsonResponse(payload, json_dumps_params={'ensure_ascii': False})
-
-
-def get_geojson():
-    geo_json = {
-      "type": "FeatureCollection",
-      "features": []
-    }
-    Places = Place.objects.all()
-    for i in range(len(Places)):
-        feature = {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [Places[i].longitude, Places[i].latitude]
-          },
-          "properties": {
-            "title": Places[i].title,
-            "placeId": "moscow_legends",
-            "detailsUrl": "static/places/moscow_legends.json"
-          }
-        }
-        geo_json["features"].append(feature)
-    print(len(geo_json['features']))
-    return geo_json
-
-
-def show(request):
-    template = loader.get_template('index.html')
-    context = {
-                'geo_json': get_geojson(),
-               }
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
