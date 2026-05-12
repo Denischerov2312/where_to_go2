e270995d1e8b913c1abd574080403885c9d960f3
Denischerov2312 <107198033+Denischerov2312@users.noreply.github.com>
2026-05-12 22:00:54 +0300

    Добавил проверки HTTP статуса

--- places/management/commands/load_places.py
+++ places/management/commands/load_places.py
@@ -20,22 +20,32 @@ class Command(BaseCommand):
             ):
         github_url = join(f'https://api.github.com/repos/{owner}/{repo}/contents/',
                           download_dirs)
-        response = requests.get(github_url)
-        response.raise_for_status()
-        files = response.json()
+        try:
+            response = requests.get(github_url)
+            response.raise_for_status()
+            files = response.json()
+            if 'error' in files:
+                raise requests.exceptions.HTTPError(file['error'])
+        except requests.exceptions.RequestException as e:
+            self.stdout.write(self.style.ERROR(f"Не удалось получить список файлов: {e}"))
         self.stdout.write(f"Загрузка данных json из {github_url}")
         for file_info in files:
             if file_info['name'].endswith('.json'):
-                file_response = requests.get(file_info['download_url'])
-                file_response.raise_for_status()
-                file_data = file_response.json()
-                makedirs(self.saved_path, exist_ok=True)
-                with open(join(
-                        self.saved_path,
-                        file_info['name']
-                        ), 'w', encoding='utf-8') as file:
-                    json.dump(file_data, file, ensure_ascii=False)
-                self.stdout.write(self.style.SUCCESS(f"Успешно загружено: {file_info['name']}"))
+                try:
+                    file_response = requests.get(file_info['download_url'])
+                    file_response.raise_for_status()
+                    file_data = file_response.json()
+                    if 'error' in file_data:
+                        raise requests.exceptions.HTTPError(file_data['error'])
+                    makedirs(self.saved_path, exist_ok=True)
+                    with open(join(
+                            self.saved_path,
+                            file_info['name']
+                            ), 'w', encoding='utf-8') as file:
+                        json.dump(file_data, file, ensure_ascii=False)
+                    self.stdout.write(self.style.SUCCESS(f"Успешно загружено: {file_info['name']}"))
+                except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
+                    self.stdout.write(self.style.ERROR(f'Ошибка при обработке файла: {file_info["name"]}: {e}'))
 
     def download_img(self, img_url):
         response = requests.get(img_url)
@@ -49,26 +59,34 @@ class Command(BaseCommand):
         self.stdout.write("Импорт данных в базу")
         for file_path in Path(self.saved_path).iterdir():
             if file_path.is_file():
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    place_details = json.load(file)
-                    place_obj, place_created = Place.objects.get_or_create(
-                        title=place_details['title'],
-                        short_description=place_details['description_short'],
-                        long_description=place_details['description_long'],
-                        longitude=float(place_details['coordinates']['lng']),
-                        latitude=float(place_details['coordinates']['lat']),
-                    )
-                    if place_created:
-                        self.stdout.write(self.style.SUCCESS(f'Добавлено место: {place_obj.title}'))
-                    else:
-                        self.stdout.write(self.style.WARNING(f'Место: {place_obj.title} уже есть в базе'))
-                    for number, img_url in enumerate(place_details['imgs'], 1):
-                        image_obj, created = Image.objects.get_or_create(
-                            place=place_obj,
-                            number=number
-                        )
-                        if created:
-                            image_obj.image = self.download_img(img_url)
-                            image_obj.save()
+                try:
+                    with open(file_path, 'r', encoding='utf-8') as file:
+                        place_details = json.load(file)
+                        place_obj, place_created = Place.objects.get_or_create(
+                            title=place_details['title'],
+                            defaults={
+                                'short_description': place_details['description_short'],
+                                'long_description': place_details['description_long'],
+                                'longitude': float(place_details['coordinates']['lng']),
+                                'latitude': float(place_details['coordinates']['lat']),  
+                            }
 
-                            self.stdout.write(f"Загружена картинка: {basename(image_obj.image.name)}")
+                        )
+                        if place_created:
+                            self.stdout.write(self.style.SUCCESS(f'Добавлено место: {place_obj.title}'))
+                        else:
+                            self.stdout.write(self.style.WARNING(f'Место: {place_obj.title} уже есть в базе'))
+                        for number, img_url in enumerate(place_details['imgs'], 1):
+                            try:
+                                image_obj, created = Image.objects.get_or_create(
+                                    place=place_obj,
+                                    number=number
+                                )
+                                if created:
+                                    image_obj.image = self.download_img(img_url)
+                                    image_obj.save()
+                                    self.stdout.write(f"Загружена картинка: {basename(image_obj.image.name)}")
+                            except requests.exceptions.RequestException as e:
+                                self.stdout.write(f'Ошибка загрузки картинки {img_url}: {e}')
+                except Exception as e:
+                    self.stdout.write(self.style.ERROR(f'Ошибка при обработке {basename(file_path.name)} : {e}'))
